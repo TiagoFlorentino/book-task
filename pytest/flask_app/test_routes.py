2608765e2e8516d84029402c8b2677055c21723d
tiagoflorentino42 <tiagoflorent@gmail.com>
2022-07-14 22:05:58 +0100

    Fix base testing

--- pytest/flask_app/test_routes.py
+++ pytest/flask_app/test_routes.py
@@ -1,3 +1,3 @@
 def test_request_example(client):
-    response = client.get("/posts")
-    assert b"<h2>Hello, World!</h2>" in response.data
+    response = client.get("/hello")
+    assert "Hello, World!" in str(response.data)
