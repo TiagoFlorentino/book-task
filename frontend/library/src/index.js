61b7c9c2791d6d6bbfc607058ddd6047ff02e8a2
tiagoflorentino42 <tiagoflorent@gmail.com>
2022-07-24 01:02:11 +0100

    Add book

--- frontend/library/src/index.js
+++ frontend/library/src/index.js
@@ -4,6 +4,7 @@ import ListBooks from "./pages/ListBooks";
 import ListClients from "./pages/ListClients";
 import ListCampaigns from "./pages/ListCampaigns";
 import ListPartners from "./pages/ListPartners";
+import AddBook from "./pages/AddBook";
 
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
       <Routes>
         <Route path="/">
             <Route path="list_books" element={<ListBooks />} />
+            <Route path="add_book" element={<AddBook />} />
             <Route path="list_clients" element={<ListClients />}/>
             <Route path="list_campaigns" element={<ListCampaigns />}/>
             <Route path="list_partners" element={<ListPartners />}/>
---
+++ frontend/library/src/pages/AddBook.jsx
@@ -0,0 +1,44 @@
+import React,  { useState } from "react";
+import '../App.css';
+
+function AddBook() {
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ title: inputs.title })
+    };
+    fetch('http://127.0.0.1:8000/add_book', requestOptions)
+        .then(response => response.json())
+        .then((json) => alert("Request Completed"))
+  }
+
+  return (
+  <div className = "AddBook">
+     <h1> Add a new book to the library </h1>
+         <form onSubmit={handleSubmit}>
+      <label>Enter the tile of the book:
+      <input
+        type="text"
+        name="title"
+        value={inputs.title || ""}
+        onChange={handleChange}
+      />
+      </label>
+        <input type="submit" />
+    </form>
+    </div>
+
+  )
+}
+
+export default AddBook;
